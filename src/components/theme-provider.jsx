31565baf47c5fb3808ae2b1caf7f48bce451ba83
Pvvirg <pvvirg16@gmail.com>
2024-01-30 22:31:51 -0300

    Esquematização do projeto

---
+++ src/components/theme-provider.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import PropTypes from "prop-types";
+
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
